a66f38cf237e2c86b6325d7e813ecf236949e267
Sean-Griffith <DarkmatterV2@gmail.com>
2021-07-12 03:09:18 -0700

    Encapsulate bot setup in function

--- bot_main.py
+++ bot_main.py
@@ -2,17 +2,17 @@ import discord
 from discord.ext import tasks, commands
 import asyncio
 import os
-from bot_setup import load_cogs
+import bot_setup
 
-if("bot_token" in os.environ):
-    bot_token = os.environ["bot_token"]
 
-#Set the command prefix for users trying to interact with the bot
-bot = commands.Bot(command_prefix="?", case_insensitive=True)
+bot = None
 
 try:
+    # Load settings
+    bot, bot_token = bot_setup.define_settings()
+    
     # Load modules/cogs with additional functionality for the bot before running
-    load_cogs(bot)
+    bot_setup.load_cogs(bot)
     if(bot_token):
         bot.run(bot_token)
     else:
--- bot_setup.py
+++ bot_setup.py
@@ -1,9 +1,25 @@
+import discord
+from discord.ext import tasks, commands
+import asyncio
 import os
+
+def define_settings():
+    bot_token = ""
+    if("bot_token" in os.environ):
+        bot_token = os.environ["bot_token"]
+
+    # Set the command prefix for users trying to interact with the bot
+    bot = commands.Bot(command_prefix="?", case_insensitive=True)
+
+    # Remove default help command for custom implementation
+    bot.remove_command("help")
+
+    return bot, bot_token
 
 def load_cogs(bot):
 # Load Cogs
     for filename in os.listdir("./cogs"):
-        if filename.endswith(".py"):
+        if (filename.endswith(".py") and not filename.startswith("__")):
             # Attempt to load the extension
             try:
                 bot.load_extension("cogs.{}".format(filename[:-3]))
